99224a20eff2ec51e8fce3640347155d28b07f3c
Michael <michael.hladky@push-based.io>
2023-06-03 23:09:59 +0200

    init nx project and provide ng cli features

--- libs/nx-plugin/jest.config.ts
+++ libs/nx-plugin/jest.config.ts
@@ -1,11 +1,10 @@
 /* eslint-disable */
 export default {
-  displayName: 'main-app',
+  displayName: 'nx-plugin',
   preset: '../../jest.preset.js',
-  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/main-app',
+  coverageDirectory: '../../coverage/libs/nx-plugin',
 };
